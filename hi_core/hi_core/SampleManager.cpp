d6dec5e6e4ae0d02cc41c6ae684d2cf55ee62686
bmalumphy <bryanmalumphy@gmail.com>
2025-08-20 15:28:22 -0400

    Revert "Fix for issue with IRs and Memory Leakage on setFile(file:)"
    This reverts commit 1245721700197015b88ca33f21ad68aa08c2ffe6.

--- hi_core/hi_core/SampleManager.cpp
+++ hi_core/hi_core/SampleManager.cpp
@@ -82,10 +82,10 @@ void MainController::SampleManager::setShouldSkipPreloading(bool skip)
 
 void MainController::SampleManager::preloadEverything()
 {
-    if(skipPreloading)
+    if(!skipPreloading)
         return;
 
-	skipPreloading = true;
+	skipPreloading = false;
 
 	LockHelpers::freeToGo(mc);
 
